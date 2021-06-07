4192ab0460340c9fb3b811d9bb063564cfbd4d13
masaaa1011 <elle.hedgehog.diaries.26masa08@gmail.com>
2021-06-08 04:21:30 +0900

    added sample files

--- web/src/pages/using-typescript.tsx
+++ web/src/pages/using-typescript.tsx
@@ -1,6 +1,6 @@
 // If you don't want to use TypeScript you can delete this file!
 import * as React from "react"
-import { PageProps, Link, graphql } from "gatsby"
+import { PageProps, graphql } from "gatsby"
 
 type DataProps = {
   site: {
